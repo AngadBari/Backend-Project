f92c909bd7bbbe7e130d41ea3caabe8bc3d7b619
Angad Bari <angadbari2@gmail.com>
2025-05-20 09:57:01 +0530

    connect to MongoDB

---
+++ src/index.js
@@ -0,0 +1,49 @@
+//require('dotenv').config({path:'./env'})-->wast way
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path:'./env'
+})
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//1st Method 
+ 
+/*
+import express from "express"
+const app =express()
+
+(async()=>{
+ try{
+     mongoose.connect(`${process.env.MONGO_URI}/${DB_Name}`);
+     app.on("error",(error)=>{
+        console.log("ERROR:",error)
+     })
+
+     app.listen(process.env.port,()=>{
+        console.log(`App is listen on port ${process.env.port}`)
+     })
+ }
+ catch(eror){
+  console.log("ERROR:",eror)
+  throw err
+ }
+
+})()*/
